18138d0e1140d79caa729336ff6e2ddd81fb1143
takehilo <takehilo.kaneko@gmail.com>
2018-08-04 15:59:32 +0900

    Initial Commit

---
+++ RxSwiftTestPatterns/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwiftTestPatterns
+//
+//  Created by 金子雄大 on 2018/08/04.
+//  Copyright © 2018年 Takehiro Kaneko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
