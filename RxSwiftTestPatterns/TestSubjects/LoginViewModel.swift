013e22a9d1dcdca70481e6d435c2abce33378956
takehilo <takehilo.kaneko@gmail.com>
2018-09-07 07:24:50 +0900

    add login form test

--- RxSwiftTestPatterns/TestSubjects/LoginViewModel.swift
+++ RxSwiftTestPatterns/TestSubjects/LoginViewModel.swift
@@ -3,8 +3,17 @@ import RxCocoa
 
 class LoginViewModel {
     let email = BehaviorRelay<String>(value: "")
+    let password = BehaviorRelay<String>(value: "")
 
     var isValidEmail: Driver<Bool> {
         return email.asDriver().map { !$0.isEmpty }
+    }
+    
+    var isValidPassword: Driver<Bool> {
+        return password.asDriver().map { !$0.isEmpty }
+    }
+    
+    var isValidForm: Driver<Bool> {
+        return Driver.combineLatest(isValidEmail, isValidPassword).map { $0 && $1 }
     }
 }
--- RxSwiftTestPatternsTests/LoginViewModelSpec.swift
+++ RxSwiftTestPatternsTests/LoginViewModelSpec.swift
@@ -41,5 +41,39 @@ class LoginViewModelSpec: QuickSpec {
                 ]))
             }
         }
+        
+        describe("isValidForm") {
+            it("should be true when both email and password are valid") {
+                let xs1 = scheduler.createHotObservable([
+                    Recorded.next(10, ""),
+                    Recorded.next(30, "a@example.com"),
+                    Recorded.next(50, "")
+                ])
+                
+                let xs2 = scheduler.createHotObservable([
+                    Recorded.next(20, ""),
+                    Recorded.next(40, "passw0rd"),
+                    Recorded.next(60, "")
+                ])
+                
+                xs1.bind(to: loginViewModel.email).disposed(by: disposeBag)
+                xs2.bind(to: loginViewModel.password).disposed(by: disposeBag)
+                
+                let observer = scheduler.createObserver(Bool.self)
+                loginViewModel.isValidForm.drive(observer).disposed(by: disposeBag)
+                
+                scheduler.start()
+                
+                expect(observer.events).to(equal([
+                    Recorded.next(0, false),
+                    Recorded.next(10, false),
+                    Recorded.next(20, false),
+                    Recorded.next(30, false),
+                    Recorded.next(40, true),
+                    Recorded.next(50, false),
+                    Recorded.next(60, false)
+                ]))
+            }
+        }
     }
 }
